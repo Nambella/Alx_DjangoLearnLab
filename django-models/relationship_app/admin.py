732ef597b30d0dd88a1deae1e45a8139159ac798
Paulose <pthiopia@outlook.com>
2024-08-18 20:54:36 +0300

    django-models

--- django-models/relationship_app/admin.py
+++ django-models/relationship_app/admin.py
@@ -12,3 +12,8 @@ def admin_view(request):
     # Your admin view logic here
     return render(request, 'admin_view.html')
 
+def create_admin_user(username, password):
+    user = user.objects.create_user(username=username, password=password)
+    user_profile = user_profile.objects.create(user=user, role='Admin')
+    user_profile.can_view_admin_panel = True
+    user_profile.save()
