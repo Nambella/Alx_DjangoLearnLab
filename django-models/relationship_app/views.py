cbf8cd15c22fe9aa9823f3a19522267b38ea30c3
Paulose <pthiopia@outlook.com>
2024-08-18 17:49:17 +0300

    django-models

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,14 +2,15 @@
 from django.views.generic.detail import DetailView
 from django.views.generic import ListView
 from .models import Library, Book
+from .models import Book
+
 
 class LibraryDetailView(ListView):
     model = Book
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'books'
 
     def get_queryset(self):
         library_id = self.kwargs['library_id']
         return Book.objects.all.filter(library__id=library_id)
 "relationship_app/list_books.html", "Book.objects.all()"
-["relationship_app/library_detail.html"]
