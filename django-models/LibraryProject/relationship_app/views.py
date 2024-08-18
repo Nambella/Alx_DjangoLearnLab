87eaaa0fc3ce3c4412152e475d437e81126fe508
Paulose <pthiopia@outlook.com>
2024-08-18 13:47:56 +0300

    views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 # relationship_app/views.py
 from django.shortcuts import render
-from .models import models # Import your model(s)
+from .models import Book  # Import your model(s)
 
-def my_view(request):
-    # Your view logic here
-    queryset = models.objects.all()
-    return render(request, 'your_template.html', {'queryset': queryset})
+def list_books(request):
+    books = books.objects.all()
+    return render(request, 'books_list.html', {'books': books})
