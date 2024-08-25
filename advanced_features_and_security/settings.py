a49cb5732bd8556105bfbc8936ba4039db69c0da
Paulose <pthiopia@outlook.com>
2024-08-25 06:54:59 +0300

    LibraryProject

--- advanced_features_and_security/settings.py
+++ advanced_features_and_security/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# Set the custom user model as the default
-AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
+
