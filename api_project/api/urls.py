a3b53274487439c64647d3cb7d52d7647312fe13
Paulose <pthiopia@outlook.com>
2024-09-01 16:55:41 +0300

    urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 
-router = "DefaultRouter ()"
+router = DefaultRouter ()
 router.register(r'books', BookViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('DefaultRouter', include(router.urls)),
 ]
