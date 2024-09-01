cb8ca417a04f8de2ab0705d0607a400e5bdc6086
Paulose <pthiopia@outlook.com>
2024-09-01 23:31:40 +0300

    api_project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,11 +3,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter ()
+["DefaultRouter()"]
 router.register(r'books', BookViewSet)
 
 urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('api/', include(router.urls)),
-    ["DefaultRouter()"]
-]
+  ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,9 +2,17 @@
 
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+"""
+BookViewSet handles all CRUD operations for Book model.
+Token authentication is required to access these endpoints.
+Permissions:
+- IsAuthenticated: Only authenticated users can access these endpoints.
+"""
