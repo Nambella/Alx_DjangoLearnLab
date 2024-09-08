725477efded432585a783c38e446acbac3b017c3
Paulose <pthiopia@outlook.com>
2024-09-08 21:55:25 +0300

    test_views.py

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -13,17 +13,20 @@ class BookTests(APITestCase):
         data = {'title': 'New Book', 'publication_year': 2024, 'author': self.author.id}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], 'New Book')
 
     def test_retrieve_book(self):
         url = reverse('book-retrieve-update-destroy', args=[self.book.id])
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Harry Potter')
 
     def test_update_book(self):
         url = reverse('book-retrieve-update-destroy', args=[self.book.id])
         data = {'title': 'Updated Book', 'publication_year': 2023, 'author': self.author.id}
         response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Updated Book')
 
     def test_delete_book(self):
         url = reverse('book-retrieve-update-destroy', args=[self.book.id])
@@ -34,13 +37,8 @@ class BookTests(APITestCase):
         url = reverse('book-list-create') + '?title=Harry'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreater(len(response.data), 0)
 
     def test_search_books(self):
         url = reverse('book-list-create') + '?search=Harry'
         response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_order_books (self):
-        url = reverse('book-list-create') + '?ordering=title'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
