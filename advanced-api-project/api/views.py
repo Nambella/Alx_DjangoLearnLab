f464fe6f5fdbf2b06f70e3a1b47dfdacb64e881e
Paulose <pthiopia@outlook.com>
2024-09-08 21:13:38 +0300

    views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,8 +5,7 @@ from django.shortcuts import render
 from rest_framework import serializers
 
 from api.models import Book
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 class BookCreate(Generic.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer # type: ignore
