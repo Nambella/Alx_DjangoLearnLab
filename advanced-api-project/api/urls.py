069cd30479570bcd9d68a3d04be485f1a30f2c79
Paulose <pthiopia@outlook.com>
2024-09-08 21:11:29 +0300

    urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,3 +5,5 @@ urlpatterns = [
     path('books/', BookListCreate.as_view(), name='book-list-create'),
     path('books/<int:pk>/', BookRetrieveUpdateDestroy.as_view(), name='book-retrieve-update-destroy'),
 ]
+[path ("books/create.as-views()", name= 'books-list-create'),
+       "books/update", "books/delete"]
