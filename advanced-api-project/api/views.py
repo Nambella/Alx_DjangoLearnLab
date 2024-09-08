8261ba55e27deb54fbea838099e7e124f868a687
Paulose <pthiopia@outlook.com>
2024-09-08 20:38:46 +0300

    all changes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,29 @@
+from datetime import datetime
+from typing import Generic
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import serializers
+
+from api.models import Book
+from rest_framework.permissions import IsAuthenticated
+
+class BookCreate(Generic.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer # type: ignore
+    permission_classes = [IsAuthenticated]
+
+class BookUpdate(Generic.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer # type: ignore
+    permission_classes = [IsAuthenticated]
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def validate_publication_year(self, value):
+        if value > datetime.date.today().year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
