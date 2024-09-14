293088a01b7a561e8eb39d434b696e8aa0f3440f
Paulose <pthiopia@outlook.com>
2024-09-15 02:34:19 +0300

    forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField   class Meta:
+    email = forms.EmailField   
+    class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
