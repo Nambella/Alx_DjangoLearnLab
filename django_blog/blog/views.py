1f5c8876ed1f1752c55de7d8b166662a96a25105
Paulose <pthiopia@outlook.com>
2024-09-15 03:36:01 +0300

    views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
