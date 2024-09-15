a5061a2d8772f2d0a05accde61dac532582ecc6d
Paulose <pthiopia@outlook.com>
2024-09-15 23:15:17 +0300

    django_blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,15 +3,22 @@ from .models import posts
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Comment
+from .models import Post, Tag
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField   
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 class PostForm(forms.ModelForm):
+    
     class Meta:
         model = posts
         fields = ['title', 'content']
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), required=False)
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 from django.contrib.auth.models import User
-
+from taggit.managers import TaggableManager
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager()
+    tags = models.ManyToManyField(tags, related_name='posts')
 class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -19,9 +21,5 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-class Post(models.Model):
-    # existing fields
-    tags = models.ManyToManyField(Tag, related_name='posts')
-
 def __str__(self):
         return f'Comment by {self.author} on {self.post}'
