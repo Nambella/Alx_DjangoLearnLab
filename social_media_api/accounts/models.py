8ad5983026cdbcf99377ab38ef8c67b517d6f6b8
Paulose <pthiopia@outlook.com>
2024-09-19 15:00:02 +0300

    posts

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
-User = get_user_model()
+
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
