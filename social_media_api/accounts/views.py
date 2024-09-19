b1e67a21e65e6409129e0e02eecbfba7b266393a
Paulose <pthiopia@outlook.com>
2024-09-19 11:11:56 +0300

    social_media_api

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import UserSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key})
+
+class LoginView(APIView):
+    def post(self, request):
+        # Implement login logic here
+        pass
