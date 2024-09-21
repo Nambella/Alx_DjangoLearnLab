a7b41d3d78eb723cbdb4341412a9bb291578a7dc
Paulose <pthiopia@outlook.com>
2024-09-21 08:14:05 +0300

    s0cial_media_api

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,11 @@ from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
@@ -19,4 +24,17 @@ class LoginView(APIView):
     def post(self, request):
         # Implement login logic here
         pass
+User = get_user_model()
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(User, id=user_id)
+    request.user.following.add(user_to_follow)
+    return JsonResponse({'status': 'followed'})
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(User, id=user_id)
+    request.user.following.remove(user_to_unfollow)
+    return JsonResponse({'status': 'unfollowed'})
 
