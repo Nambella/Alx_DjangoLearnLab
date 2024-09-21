15a74156e8159a6d2115efba19801ff511fccc2c
Paulose <pthiopia@outlook.com>
2024-09-21 09:49:11 +0300

    social_media_api

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
+from .views import feed
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -8,4 +9,8 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+]
+
+urlpatterns = [
+    path('feed/', feed, name='feed'),
 ]
